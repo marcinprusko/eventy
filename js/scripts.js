b42ffc48def410a9ddc4a09a1b6cc4698c1ed285
marcinprusko <marcinprusko@gmail.com>
2017-03-07 22:09:45 +0100

    eventy do sprawdzenia

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+﻿// plik scripts.js
+
+var lista = document.getElementById('list');
+var add = document.getElementById('addElem');
+
+addEventListener('click', function() {
+  list.innerHTML += '<li>item</li>';
+});
+
+var itemjs = document.getElementsByTagName("item");
+console.log(itemjs);
+console.log(itemjs.length);
+
+
+
+
+
+
